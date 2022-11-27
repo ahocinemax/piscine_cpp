752b79a77c5fdc872dbc2800280a99edb4a33eae
root <root@DESKTOP-23JLMMR.localdomain>
2022-11-27 04:37:24 +0100

    05/00 ongoing

--- 5.module_5/ex00/includes/Bureaucrat.Class.hpp
+++ 5.module_5/ex00/includes/Bureaucrat.Class.hpp
@@ -13,16 +13,55 @@
 #ifndef BUREAUCRAT_CLASS_HPP
 # define BUREAUCRAT_CLASS_HPP
 
+# include <iostream>
 # include <string>
 
 class Bureaucrat
 {
 	private:
+		int					range;
 		const std::string	name;
 
 	public:
-		Bureaucrat(/* args */);
-		~Bureaucrat();
+		// Constructors & Destructor
+		Bureaucrat(std::string name, int range);
+		Bureaucrat(const Bureaucrat &src);
+		~Bureaucrat(void);
+		Bureaucrat(void);
+		
+		// Assignement operator
+		Bureaucrat &operator=(const Bureaucrat &rhs);
+		
+		// Getters
+		const std::string	&getName(void) const;
+		int					getGrade(void) const;
+
+		// Member functions
+		void				toTheMoon(void);
+		void				toTheFomo(void);
+
+		// Exceptions
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				GradeTooHighException(const GradeTooHighException &Copy);
+				GradeTooHighException(void);
+				~GradeTooHighException() throw();
+				GradeTooHighException &operator=(const GradeTooHighException &rhs);
+				const virtual char	*what() const throw();
+		};
+
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				GradeTooLowException(const GradeTooLowException &Copy);
+				GradeTooLowException(void);
+				~GradeTooLowException() throw();
+				GradeTooLowException &operator=(const GradeTooLowException &rhs);
+				const virtual char	*what() const throw();
+		};
 };
+
+std::ostream &operator<<(std::ostream &out, Bureaucrat const &rhs);
 
 #endif
--- 5.module_5/ex00/srcs/Bureaucrat.cpp
+++ 5.module_5/ex00/srcs/Bureaucrat.cpp
@@ -12,12 +12,70 @@
 
 #include "../includes/Bureaucrat.Class.hpp"
 
-Bureaucrat::Bureaucrat(/* args */)
+Bureaucrat::Bureaucrat(std::string name, int range) : name(name), range(range)
+{
+	if (range < 1)
+		throw Bureaucrat::GradeTooHighException();
+	if (range > 150)
+		throw Bureaucrat::GradeTooLowException();
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &src) : name(src.getName()), range(src.getGrade())
 {
 	;
 }
 
-Bureaucrat::~Bureaucrat()
+Bureaucrat::~Bureaucrat(void)
 {
 	;
+}
+
+Bureaucrat::Bureaucrat(void) : name("Basic bureaucrat"), range(150)
+{
+	;
+}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &rhs)
+{
+	if (this == &rhs)
+		return (*this);
+	range = rhs.getGrade();
+	return (*this);
+}
+
+int	Bureaucrat::getGrade(void) const
+{
+	return (this->range);
+}
+
+const std::string	&Bureaucrat::getName(void) const
+{
+	return (this->name);
+}
+
+void	Bureaucrat::toTheMoon(void)
+{
+	if (this->getGrade() < 2)
+		range -= 1;
+	else
+		throw GradeTooHighException();
+}
+
+void	Bureaucrat::toTheFomo(void)
+{
+	if (this->getGrade() > 149)
+		range += 1;
+	else
+		throw GradeTooLowException();
+
+}
+
+const char	*Bureaucrat::GradeTooHighException::what() const
+{
+	std::cout << "Grade cannot be better than 1." << std::endl;
+}
+
+const char	*Bureaucrat::GradeTooLowException::what() const
+{
+	std::cout << "Grade cannot be worst than 150." << std::endl;
 }
