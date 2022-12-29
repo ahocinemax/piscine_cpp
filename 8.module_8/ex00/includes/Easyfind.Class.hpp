02f21d01bda9bc53ab3de8392e1cc6c2241b259f
ahocine <ahocine@student.42.fr>
2022-12-29 19:07:14 +0100

    default commit message

--- 8.module_8/ex00/includes/Easyfind.Class.hpp
+++ 8.module_8/ex00/includes/Easyfind.Class.hpp
@@ -1,10 +1,36 @@
 #ifndef EASYFIND_CLASS_HPP
 # define EASYFIND_CLASS_HPP
 
-template <typename T>
-int easyfind(T *array, int value)
+# include <exception>
+# include <algorithm>
+# include <vector>
+
+class EmptyArrayException : public std::exception
 {
-    return (0);
+	public:
+		virtual const char	*what() const throw() {return ("Empty array\n");}
+};
+
+class ElementNotFoundException : public std::exception
+{
+	public:
+		virtual const char	*what() const throw() {return ("Element not found on container\n");}
+
+};
+
+template <typename T>
+typename T::iterator	easyfind(T &array, int value)
+{
+	typename T::iterator	it;
+
+	if (array.empty())
+		throw EmptyArrayException();
+	for (it = array.begin() ; it != array.end() ; it++)
+	{
+		if (*it == value)
+			return (it);
+	}
+	throw ElementNotFoundException();
 }
 
 #endif
--- 8.module_8/ex00/srcs/main.cpp
+++ 8.module_8/ex00/srcs/main.cpp
@@ -1,6 +1,29 @@
 #include "../includes/Easyfind.Class.hpp"
+#include <iostream>
+#include <list>
+
+void	ft_print(int i)
+{
+	std::cout << i << std::endl;
+}
 
 int main(void)
 {
-    return (0);
+	std::vector<int>	vct;
+	std::list<int>		lst;
+	
+	for (int i = 0 ; i <= 42 ; i++)
+		vct.push_back(i);
+
+	try
+	{
+		std::vector<int>::iterator	it_vct = easyfind(vct, 34);
+		for_each(it_vct, vct.end(), ft_print);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+	return (0);
 }
